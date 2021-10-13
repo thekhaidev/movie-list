4180d7f7973f57e833f0c797b304c0650ff9247d
Khai Tran <kqtran917@yahoo.com>
2021-10-12 22:26:13 -0500

    Refacctored App to stateful class

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import MovieList from './MovieList.jsx';
 import SearchBar from './SearchBar.jsx';
 
-const App = (props) => (
-  <div><h1>Hello World!</h1>
-    {/* {console.log(props.data)} */}
-    <SearchBar />
-    <MovieList movies = {props.data}/>
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-);
 
+  render() {
+    return (
+      <div><h1>Hello World!</h1>
+        {/* {console.log(props.data)} */}
+        <SearchBar />
+        <MovieList movies = {this.props.data}/>
+      </div>
+    );
+  }
+}
 
 
 export default App;
