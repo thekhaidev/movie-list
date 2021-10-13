7079d0e8ce6b066460ebe0ce3ec4ebcdd776f7a8
Khai Tran <kqtran917@yahoo.com>
2021-10-12 22:21:50 -0500

    Implemented searchbar component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import MovieList from './MovieList.jsx';
+import SearchBar from './SearchBar.jsx';
 
 const App = (props) => (
-  <div>Hello World!
+  <div><h1>Hello World!</h1>
     {/* {console.log(props.data)} */}
+    <SearchBar />
     <MovieList movies = {props.data}/>
   </div>
 
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchBar = () => (
+  <div>
+    <label for="search">Search:</label>
+    <input type="text" id="search" name="search" required size="10" />
+  </div>
+
+
+)
+
+export default SearchBar
